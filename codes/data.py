26b21181052ea2a62eb42a4f9d6b2e56b066ad16
daoyuan98 <wgz0117@qq.com>
2020-05-05 19:40:32 +0800

    update for with comments, leaderboard:0.87469

--- codes/data.py
+++ codes/data.py
@@ -3,83 +3,86 @@ import pandas as pd
 
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
-keys = {'age', 'workclass', 'fnlwgt', 'education', 'education-num',
-       'marital-status', 'occupation', 'relationship', 'sex', 'capital-gain',
-       'capital-loss', 'hours-per-week', 'native-country'}
+# keys = {'age', 'workclass', 'fnlwgt', 'education', 'education-num',
+#        'marital-status', 'occupation', 'relationship', 'sex', 'capital-gain',
+#        'capital-loss', 'hours-per-week', 'native-country'}
 
-non_continuous_keys = {'workclass', 'education', 'marital-status', 'occupation', 
-                       'relationship', 'sex', 'native-country'}
+# non_continuous_keys = {'workclass', 'education', 'marital-status', 'occupation', 
+#                        'relationship', 'sex', 'native-country'}
 
-non_continuous_key_values = {
-     'workclass': [
-        ' Federal-gov', ' Local-gov', ' Private', ' Self-emp-inc', ' Self-emp-not-inc', 
-        ' State-gov', ' Without-pay', ' Never-worked'
-        ],
-     'education': [
-        ' 10th', ' 11th', ' 12th', ' 1st-4th', ' 5th-6th', ' 7th-8th',
-        ' 9th', ' Assoc-acdm', ' Assoc-voc', ' Bachelors', ' Doctorate',
-        ' HS-grad', ' Masters', ' Preschool', ' Prof-school',
-        ' Some-college'
-        ],
-     'marital-status': [
-        ' Divorced', ' Married-AF-spouse', ' Married-civ-spouse',
-        ' Married-spouse-absent', ' Never-married', ' Separated',
-        ' Widowed'
-        ],
-     'occupation': [' Adm-clerical', ' Armed-Forces', ' Craft-repair',
-       ' Exec-managerial', ' Farming-fishing', ' Handlers-cleaners',
-       ' Machine-op-inspct', ' Other-service', ' Priv-house-serv',
-       ' Prof-specialty', ' Protective-serv', ' Sales', ' Tech-support',
-       ' Transport-moving'
-       ],
-     'relationship': [' Husband', ' Not-in-family', ' Other-relative', ' Own-child',
-       ' Unmarried', ' Wife'
-       ],
-     'sex': [
-        ' Female', ' Male'
-       ], 
-     'native-country': [
-        ' Cambodia', ' Canada', ' China', ' Columbia', ' Cuba',
-        ' Dominican-Republic', ' Ecuador', ' El-Salvador', ' England',
-        ' France', ' Germany', ' Greece', ' Guatemala', ' Haiti',
-        ' Holand-Netherlands', ' Honduras', ' Hong', ' Hungary', ' India',
-        ' Iran', ' Ireland', ' Italy', ' Jamaica', ' Japan', ' Laos',
-        ' Mexico', ' Nicaragua', ' Outlying-US(Guam-USVI-etc)', ' Peru',
-        ' Philippines', ' Poland', ' Portugal', ' Puerto-Rico',
-        ' Scotland', ' South', ' Taiwan', ' Thailand', ' Trinadad&Tobago',
-        ' United-States', ' Vietnam', ' Yugoslavia'
-      ]
-}
+# non_continuous_key_values = {
+#      'workclass': [
+#         ' Federal-gov', ' Local-gov', ' Private', ' Self-emp-inc', ' Self-emp-not-inc', 
+#         ' State-gov', ' Without-pay', ' Never-worked'
+#         ],
+#      'education': [
+#         ' 10th', ' 11th', ' 12th', ' 1st-4th', ' 5th-6th', ' 7th-8th',
+#         ' 9th', ' Assoc-acdm', ' Assoc-voc', ' Bachelors', ' Doctorate',
+#         ' HS-grad', ' Masters', ' Preschool', ' Prof-school',
+#         ' Some-college'
+#         ],
+#      'marital-status': [
+#         ' Divorced', ' Married-AF-spouse', ' Married-civ-spouse',
+#         ' Married-spouse-absent', ' Never-married', ' Separated',
+#         ' Widowed'
+#         ],
+#      'occupation': [' Adm-clerical', ' Armed-Forces', ' Craft-repair',
+#        ' Exec-managerial', ' Farming-fishing', ' Handlers-cleaners',
+#        ' Machine-op-inspct', ' Other-service', ' Priv-house-serv',
+#        ' Prof-specialty', ' Protective-serv', ' Sales', ' Tech-support',
+#        ' Transport-moving'
+#        ],
+#      'relationship': [' Husband', ' Not-in-family', ' Other-relative', ' Own-child',
+#        ' Unmarried', ' Wife'
+#        ],
+#      'sex': [
+#         ' Female', ' Male'
+#        ], 
+#      'native-country': [
+#         ' Cambodia', ' Canada', ' China', ' Columbia', ' Cuba',
+#         ' Dominican-Republic', ' Ecuador', ' El-Salvador', ' England',
+#         ' France', ' Germany', ' Greece', ' Guatemala', ' Haiti',
+#         ' Holand-Netherlands', ' Honduras', ' Hong', ' Hungary', ' India',
+#         ' Iran', ' Ireland', ' Italy', ' Jamaica', ' Japan', ' Laos',
+#         ' Mexico', ' Nicaragua', ' Outlying-US(Guam-USVI-etc)', ' Peru',
+#         ' Philippines', ' Poland', ' Portugal', ' Puerto-Rico',
+#         ' Scotland', ' South', ' Taiwan', ' Thailand', ' Trinadad&Tobago',
+#         ' United-States', ' Vietnam', ' Yugoslavia'
+#       ]
+# }
 
-def one_hot(data, sett):
-    continuous_keys = keys - non_continuous_keys
-    res = pd.DataFrame(data[continuous_keys])
-    # print(res)
-    if sett == "train":
-        label = pd.DataFrame(data['exceeds50K'])
+# def one_hot(data, sett):
+#     continuous_keys = keys - non_continuous_keys
+#     res = pd.DataFrame(data[continuous_keys])
+#     if sett == "train":
+#         label = pd.DataFrame(data['exceeds50K'])
 
-    one_hot_components = []
-    for key in non_continuous_keys:
-        one_hot_encoder = OneHotEncoder()
-        label_encoder = LabelEncoder()
+#     one_hot_components = []
+#     for key in non_continuous_keys:
+#         one_hot_encoder = OneHotEncoder()
+#         label_encoder = LabelEncoder()
 
-        label_encoder_labels = label_encoder.fit_transform(data[key])
-        feature_arr = one_hot_encoder.fit_transform(data[[key]]).toarray()
+#         label_encoder_labels = label_encoder.fit_transform(data[key])
+#         feature_arr = one_hot_encoder.fit_transform(data[[key]]).toarray()
 
-        new_labels = list(label_encoder.classes_)
-        features = pd.DataFrame(feature_arr, columns=new_labels)
-        one_hot_components.append(features) 
-    if sett == "train":
-        ret = pd.concat([res, *one_hot_components, label], axis=1)
-    else:
-        ret = pd.concat([res, *one_hot_components], axis=1)
-    return ret
+#         new_labels = list(label_encoder.classes_)
+#         features = pd.DataFrame(feature_arr, columns=new_labels)
+#         one_hot_components.append(features) 
+#     if sett == "train":
+#         ret = pd.concat([res, *one_hot_components, label], axis=1)
+#     else:
+#         ret = pd.concat([res, *one_hot_components], axis=1)
+#     return ret
 
 def load_data(sett, n_fold=20, start_fold=19, missing_value='drop', do_one_hot=True, numpy=True):
     file_name = "../{}.csv".format(sett)
     data_whole = pd.read_csv(file_name, keep_default_na=True)
+
+    # to handle missing value
     data_whole = data_whole.replace(' ?', np.nan)
 
+    # handle missing value in different ways
+    # current used: mean
     if missing_value == 'drop':
         print("drop missing value")
         data_na_dropped = data_whole.dropna(how='any')
@@ -93,10 +96,10 @@ def load_data(sett, n_fold=20, start_fold=19, missing_value='drop', do_one_hot=T
     if missing_value == 'mean':
         print('fill missing value with mean')
         data = data_whole.fillna(data_whole.mean())
-        
         data = data_whole.fillna(method='ffill')
         data = data.fillna(method='bfill')
 
+    # convert categorical feats to one hot
     if do_one_hot:
         data_whole =  pd.get_dummies(data)
 
@@ -104,8 +107,11 @@ def load_data(sett, n_fold=20, start_fold=19, missing_value='drop', do_one_hot=T
         train_index = []
         valid_index = []
         fold_index = 0
+
+        # divide data into n_fold splits by hand
         one_fold = data_whole.shape[0] // n_fold
         
+        # get index for train and valid
         for i in range(data_whole.shape[0]):
             if (i + 1) % one_fold == 0:
                 fold_index += 1
@@ -117,36 +123,35 @@ def load_data(sett, n_fold=20, start_fold=19, missing_value='drop', do_one_hot=T
         if missing_value == "fill" and ' Never-worked' in data_whole.keys():
             data_whole = data_whole.drop(columns=[' Never-worked'])
 
-        # train_data, train_label = data_whole.iloc[train_index, :-1], data_whole.iloc[train_index ,-1:]
-        # valid_data, valid_label = data_whole.iloc[valid_index, :-1], data_whole.iloc[valid_index ,-1:]
-
+        # get label
         data = data_whole.drop(columns='exceeds50K')
         label = data_whole['exceeds50K']
 
+        # get train and valid split
         train_data = data.iloc[train_index, :]
         train_label = label.iloc[train_index]
 
         valid_data = data.iloc[valid_index, :]
         valid_label = label.iloc[valid_index]
 
+        # if convert to np.array
         if numpy:
             return train_data.to_numpy(), train_label.to_numpy(), valid_data.to_numpy(), valid_label.to_numpy()
         else:
             return train_data, train_label, valid_data, valid_label
     else:
+        # get test set, no label
         if 'native-country_ Holand-Netherlands' in data_whole.keys():
             print("drop ", 'native-country_ Holand-Netherlands')
             data_whole = data_whole.drop(columns=['native-country_ Holand-Netherlands'])
-        # if 'workclass_ Never-worked' in data_whole.keys():
-        #     print('drop workclass')
-        #     data_whole = data_whole.drop(columns=['workclass_ Never-worked'])
-        # print(data_whole.head())
+
         if numpy:
             return data_whole.to_numpy()
         else:
             return data_whole
 
 
+# only used for debug, ignore it.
 if __name__ == '__main__':
     train_data, train_label, valid_data, valid_label = load_data("train", missing_value='fill', numpy=False)
     test_data = load_data("test", missing_value='fill', numpy=False)
--- codes/main.py
+++ codes/main.py
@@ -5,31 +5,43 @@ from sklearn.linear_model import LogisticRegressionCV
 from sklearn.metrics import accuracy_score, f1_score
 
 def assemble_predict(models, datas, weights=None, threshold=0.5):
+    
+    '''
+        models: list of classifiers
+        datas: data for evaluation for each model
+        weights: weights for different classifiers
+        threshold: pred 1 if average > threshold, 0 otherwise
+    '''
 
+    # if not weights, then average
     if weights is None:
         weights = [1.] * len(models)
     pred = np.zeros((datas[0].shape[0], ))
 
+    # average each output
     for i, model in enumerate(models):
         pred = pred + model.predict(datas[i])
-
     pred = pred / sum(weights)
     res = np.zeros((datas[0].shape[0], ))
-    res[pred >= threshold] = 1
 
+    # output from threshold
+    res[pred >= threshold] = 1
     return res
 
 
+# get accuracy between prediction and ground truth
 def get_acc(pred, gt):
     pred = pred.reshape((pred.shape[0], ))
     gt = gt.reshape((gt.shape[0], ))
     return accuracy_score(gt, pred)
 
+# get f1 score between prediction and ground truth
 def get_f1_score(pred, gt):
     pred = pred.reshape((pred.shape[0], ))
     gt = gt.reshape((gt.shape[0], ))
     return accuracy_score(gt, pred)
 
+# get score of f1 or accuracy
 def get_score(clf, data, label, metrics='f1'):
     pred = clf.predict(data)
     if metrics == 'f1':
@@ -37,10 +49,12 @@ def get_score(clf, data, label, metrics='f1'):
     if metrics == 'acc':
         return get_f1_score(pred, label)
 
+# get model name
 def get_name(clf):
     raw = str(clf)
     return raw[:raw.index('(')]
 
+# print predict result to csv file
 def output(pred, file):
     file_path = '../output/{}.csv'.format(file)
     with open(file_path, 'w+') as f:
@@ -48,9 +62,10 @@ def output(pred, file):
         for i in range(len(pred)):
             f.write('{},{}\n'.format(i+1, int(pred[i])))
 
+# not used for xgboost
+# normalize each dim of data to 0 - 1
 def scale(split1, split2, split3):
     whole = np.vstack([split1, split2, split3])
-    # print(whole.shape)
     for i in range(6):
         max_ = np.max(whole[:, i])
         min_ = np.min(whole[:, i])
@@ -61,22 +76,19 @@ def main():
     
     print('--'*50)
 
+    # load data
     train_data, train_label, valid_data, valid_label = load_data("train", n_fold=30,  missing_value='mean', do_one_hot=True)
     test_data = load_data("test", missing_value='mean', do_one_hot=True)
 
-    # train_scaled, test_scaled, valid_scaled = scale(train_data, test_data, valid_data)
-    # exit(0)
-    # print(train_scaled.shape, test_scaled.shape, valid_scaled.shape)
-
+    # reshape label to fit sklearn api
     train_label = train_label.reshape((train_label.shape[0], ))
     valid_label = valid_label.reshape((valid_label.shape[0], ))
 
 
-    # np.random.seed(19)
     random_seeds = [9, 19, 29, 199, 299, 1999, 2999, 1919]
-    print(train_data.shape)
-    models = []
 
+    # contains models for final prediction
+    models = []
     def eval_model(clf, train_data, valid_data, add=True, metric='f1'):
         acc_on_train = get_score(clf, train_data, train_label, metric)
         acc_on_valid = get_score(clf, valid_data, valid_label, metric)
@@ -90,63 +102,48 @@ def main():
             models.append(clf)
         return acc_on_valid
     
+    # not used
+    # get lr model
     def lr_model():
         from sklearn.linear_model import LogisticRegressionCV
         clf = LogisticRegressionCV(Cs=[1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100, 1e3, 1e4], cv=20, random_state=np.random.randint(1, 10000), max_iter=1000).fit(train_data, train_label)
         eval_model(clf, train_data, valid_data)
 
+    # not used
     def mlp_model():
         from sklearn.neural_network import MLPClassifier
         clf = MLPClassifier(hidden_layer_sizes=(15), alpha=0.01, random_state=np.random.randint(1, 10000), max_iter=10000).fit(train_scaled, train_label)
         eval_model(clf, train_scaled, valid_scaled)
 
+    # not used
     def svm_model():
         from sklearn.svm import SVC
         clf = SVC().fit(train_data, train_label)
         eval_model(clf)
 
+    # not used
     def random_forest_model(depth=14):
         from sklearn.ensemble import RandomForestClassifier
         clf = RandomForestClassifier(max_depth=depth, n_estimators=50, random_state=random_seeds.pop()).fit(train_data, train_label)
         eval_model(clf, train_data, valid_data)
 
+    # not used
     def adaboost_classifier():
         from sklearn.ensemble import AdaBoostClassifier
         clf = AdaBoostClassifier(n_estimators=50, random_state=random_seeds.pop()).fit(train_data, train_label)
         eval_model(clf)
 
+    # the only func used
     def xgboost_classifier(n_est=220, depth=3):
         from xgboost import XGBClassifier
         clf = XGBClassifier(n_estimators=n_est, max_depth=depth, random_state=random_seeds.pop()).fit(train_data, train_label)
         eval_model(clf, train_data, valid_data)
 
-
     print('--'*50)
-
-    # define models
-    # lr_model()
-    # mlp_model()
-    # mlp_model()
-    # mlp_model()
-    # svm_model()
-
-    # random_forest_model(12)
-    # random_forest_model(11)
-    # random_forest_model(12)
-    # random_forest_model(13)
-    # random_forest_model(14)
-
-    # adaboost_classifier()
 
     xgboost_classifier(220, 3)
     xgboost_classifier(210, 3)
     xgboost_classifier(230, 3)
-    # xgboost_classifier(100, 4)
-    # xgboost_classifier(1000, 3)
-    
-    # final model
-    # random_forest_model(14)
-    # xgboost_classifier()
 
     valid_pred = assemble_predict(models, [valid_data]*len(models))
     print("assemble acc on valid: ", get_acc(valid_pred, valid_label))
